78b6a2a164bc865fbaa38c612b09db11208fe77b
zh_eastsun <zh_eastsun@163.com>
2019-11-22 11:30:17 +0800

    commit change

--- example/src/creational/simplefactory/Client.kt
+++ example/src/creational/simplefactory/Client.kt
@@ -4,8 +4,10 @@ class Client {
     companion object{
         @JvmStatic
         fun main(args: Array<String>){
+            // 实例化一个游戏本
             val gameComputer = Foxconn().createComputer(RepublicOfGamers::class.java)
             gameComputer.work()
+            // 实例化一个办公本
             val officialComputer = Foxconn().createComputer(MacBook::class.java)
             officialComputer.work()
         }
