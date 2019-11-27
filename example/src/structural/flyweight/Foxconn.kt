e675cc4812e126a6c5556427addf7ca2bc4a0754
zh-eastsun <zh_eastsun@163.com>
2019-11-27 14:58:45 +0800

    更新享元模式用例中的代码

--- example/src/structural/flyweight/Foxconn.kt
+++ example/src/structural/flyweight/Foxconn.kt
@@ -10,9 +10,8 @@ class Foxconn {
             return if (thinkPad.containsKey(model)) {
                 thinkPad[model]!!
             } else {
-                val newThinkPad = ThinkPad(model)
-                thinkPad[model] = newThinkPad
-                newThinkPad
+                thinkPad[model] = ThinkPad(model)
+                thinkPad[model]!!
             }
             println("${thinkPad.size}")
         }
