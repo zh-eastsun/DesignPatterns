8ba2e4ec7d12e82debc0a7022a116c0e124a140f
zh-eastsun <zh_eastsun@163.com>
2019-11-27 14:53:02 +0800

    修复用例中代码的错误

--- example/src/structural/flyweight/Client.kt
+++ example/src/structural/flyweight/Client.kt
@@ -1,5 +1,7 @@
 package structural.flyweight
 
+import kotlin.system.exitProcess
+
 /**
  * 负责调用的客户端类
  */
@@ -7,10 +9,17 @@ class Client{
     companion object{
         @JvmStatic
         fun main(args:Array<String>){
-            var thinkPadX1 = Foxconn.getThinkPad("ThinkPad-X1")
+            val thinkPadX1 = Foxconn.getThinkPad("ThinkPad-X1")
             thinkPadX1.doWork()
-            var thinkPadX280 = Foxconn.getThinkPad("ThinkPad-X280")
+            val thinkPadX280 = Foxconn.getThinkPad("ThinkPad-X280")
             thinkPadX280.doWork()
+            // 第二次使用时与第一次使用的是同一个对象
+            val thinkPadX = Foxconn.getThinkPad("ThinkPad-X1")
+            thinkPadX.doWork()
+            println("${thinkPadX1 === thinkPadX}")
+
+            exitProcess(0)
+
         }
     }
 }
--- example/src/structural/flyweight/Foxconn.kt
+++ example/src/structural/flyweight/Foxconn.kt
@@ -7,13 +7,14 @@ class Foxconn {
     companion object {
         private val thinkPad = HashMap<String, ThinkPad>()
         fun getThinkPad(model: String): ThinkPad {
-            if (thinkPad.containsKey(model)) {
-                return thinkPad.get(model)!!
-            } else run {
-                var newThinkPad = ThinkPad(model)
-                thinkPad.put(model, ThinkPad(model))
-                return newThinkPad
+            return if (thinkPad.containsKey(model)) {
+                thinkPad[model]!!
+            } else {
+                val newThinkPad = ThinkPad(model)
+                thinkPad[model] = newThinkPad
+                newThinkPad
             }
+            println("${thinkPad.size}")
         }
     }
 }
