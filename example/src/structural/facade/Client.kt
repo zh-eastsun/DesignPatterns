c70030dc3c6b2b1e3c470c253106c4232edd6d66
zh-eastsun <zh_eastsun@163.com>
2019-12-05 11:40:20 +0800

    添加客户端类中对调用代码处的注释说明

--- example/src/structural/facade/Client.kt
+++ example/src/structural/facade/Client.kt
@@ -4,6 +4,7 @@ class Client{
     companion object{
         @JvmStatic
         fun main(args: Array<String>){
+            // 调用时对外部隐藏了内部实现，提供了统一的接口供外部调用
             val smartPhone = SmartPhone()
             smartPhone.dail()
             smartPhone.hangup()
